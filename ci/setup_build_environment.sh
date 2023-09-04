b27d54791b635031c5c9d8c9f7c3123e7cee2ec3
Ryan Burn <ryan.burn@gmail.com>
2023-09-03 19:34:51 -0700

    update ci

--- ci/setup_build_environment.sh
+++ ci/setup_build_environment.sh
@@ -6,24 +6,6 @@ apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y \
                 software-properties-common \
                 build-essential \
-                libperl-dev \
-                libatlas-base-dev \
-                gfortran \
-                cmake \
-                pkg-config \
-                gettext-base \
-                zip \
                 git \
-                ca-certificates \
                 curl \
-                automake \
-                autogen \
-                autoconf \
-                libtool \
-                gnupg2 \
-                ssh \
-                lcov \
-                vim \
-                gdb \
-                wget \
-                python
+                wget
